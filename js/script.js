90ffeca5dc7b5abf5c41ce3a1c57ab291aa23385
Ilya <ilieturcan424@gmail.com>
2023-06-08 13:42:23 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,158 @@
+/* const num = 50;
+
+if (num < 49) {
+    console.log('Неправильно');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Верно');
+} */
+
+/* for(let i = 1; i < 8; i++) {
+    console.log(i);
+} */
+
+/* function logging(a, b) {
+    console.log(a + b);
+}
+
+logging(3, 7);
+
+logging(6, 8); */
+
+
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/fourth_section/left_arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/fourth_section/right_arrow.png"></button>',
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    //Modal 
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #order, #descr').fadeOut('slow');
+    });
+    $('.button_catalog').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__title').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите ваше имя",
+                    minlength: jQuery.validator.format("Введите {0} символа")
+                },
+                phone: "Пожалуйста, введите ваш номер телефона",
+                email: {
+                    required: "Пожалуйста, введите вашу почту",
+                    email: "Ваша почта должна быть в формате name@domain.com"
+                }
+            }
+        });
+    };
+    validateForms('#consultation form');
+    validateForms('#consultation-form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+373 99999999");
+
+    $('form').submit(function (e) {
+        e.preventDefault(); // Отмена стандартного поведения браузера. При отправки формы, сайт не будет перезагружаться
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",   //Отдаем данные на сервер
+            url: "mailer/smart.php",    //Какой обработчик будет обрабатывать всю эту ситуацию 
+            data: $(this).serialize()   //Данные которые мы хотим отправить на сервер. Serialize - данные которые мы получили от пользователя необходимо подготовить перед отправкой на сервер.
+        }).done(function () {
+            $(this).find("input").val("");  //Очищаем все строки инпутов. Ставим им пустую строку.
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #descr').fadeIn('slow');
+
+            $('form').trigger('reset'); // Все мои формы должны обновится. 
+        });
+        return false;
+    });
+});
+
+
+
+/* const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    responsive: {
+        640: {
+            edgePadding: 20,
+            gutter: 20,
+            items: 1
+        },
+        700: {
+            gutter: 30
+        },
+        900: {
+            items: 1,
+        }
+    }
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
+ */
